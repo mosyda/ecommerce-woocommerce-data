d08a4987e72137fb0fbe2c077fc9224dfc95517d
Maciej Osyda <maciejosyda@seamless.se>
2016-01-15 12:29:24 +0100

    Allowed root to install wordpress

--- install-wc.sh
+++ install-wc.sh
@@ -8,8 +8,8 @@ done
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`wordpress\` CHARACTER SET utf8 COLLATE utf8_general_ci;"
 
 cd /var/www/html
-/var/www/html/wp-cli core download --version=3.5
-/var/www/html/wp-cli core config --dbname=wordpress --dbuser=root --dbpass=root
-/var/www/html/wp-cli core install --url=http://localhost:8888/wordpress/ --title=WordPress --admin_user=admin --admin_password=admin123 --admin_email=somefakeemail@fakedomain.com
+/var/www/html/wp-cli core download --version=3.5 --allow-root
+/var/www/html/wp-cli core config --dbname=wordpress --dbuser=root --dbpass=root --allow-root
+/var/www/html/wp-cli core install --url=http://localhost:8888/wordpress/ --title=WordPress --admin_user=admin --admin_password=admin123 --admin_email=somefakeemail@fakedomain.com --allow-root
 
 
