81e88ce20c7dff88d5d8fe43871c632b8e82b13a
Maciej Osyda <maciejosyda@seamless.se>
2016-01-15 10:29:14 +0100

    Added wait for mysql, changed script path to absolute

--- install-wc.sh
+++ install-wc.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -x
 
+# Wait for MySQL
+while ! mysqladmin ping -h localhost --silent; do
+    sleep 1
+done
+
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`wordpress\` CHARACTER SET utf8 COLLATE utf8_general_ci;"
 
 cd /var/www/html
-./wp-cli core download --version=3.5
-./wp-cli core config --dbname=wordpress --dbuser=root --dbpass=root
-./wp-cli core install --url=http://localhost:8888/wordpress/ --title=WordPress --admin_user=admin --admin_password=admin123 --admin_email=somefakeemail@fakedomain.com
+/var/www/html/wp-cli core download --version=3.5
+/var/www/html/wp-cli core config --dbname=wordpress --dbuser=root --dbpass=root
+/var/www/html/wp-cli core install --url=http://localhost:8888/wordpress/ --title=WordPress --admin_user=admin --admin_password=admin123 --admin_email=somefakeemail@fakedomain.com
 
 
