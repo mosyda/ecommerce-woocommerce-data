51b703a3d8cabb7b67353e5f4b128c904fa74843
Maciej Osyda <maciejosyda@seamless.se>
2016-01-15 12:59:35 +0100

    Changed port number to 8080 in install script

--- install-wc.sh
+++ install-wc.sh
@@ -15,6 +15,6 @@ mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`wordpress\` CHARACTER SET utf8
 cd /var/www/html
 /var/www/html/wp-cli core download --version=$1 --allow-root
 /var/www/html/wp-cli core config --dbname=wordpress --dbuser=root --allow-root
-/var/www/html/wp-cli core install --url=http://localhost:8888/wordpress/ --title=WordPress --admin_user=admin --admin_password=admin123 --admin_email=somefakeemail@fakedomain.com --allow-root
+/var/www/html/wp-cli core install --url=http://localhost:8080/wordpress/ --title=WordPress --admin_user=admin --admin_password=admin123 --admin_email=somefakeemail@fakedomain.com --allow-root
 
 
