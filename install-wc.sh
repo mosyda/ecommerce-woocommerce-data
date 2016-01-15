aaf2f30e08395852f36e67e2a526f74c782c9198
Maciej Osyda <maciejosyda@seamless.se>
2016-01-15 13:46:15 +0100

    Fixed siteurl, added woocommerce plugin install

--- install-wc.sh
+++ install-wc.sh
@@ -5,6 +5,11 @@ if [ -z "$1" ]
     echo "First argument should be a wordpress version (at least 3.7). No version supplied"
 fi
 
+if [ -z "$2" ]
+  then
+    echo "Second argument should be a wordpress version. No version supplied"
+fi
+
 # Wait for MySQL
 while ! mysqladmin ping -h localhost --silent; do
     sleep 1
@@ -16,5 +21,6 @@ cd /var/www/html
 /var/www/html/wp-cli core download --version=$1 --allow-root
 /var/www/html/wp-cli core config --dbname=wordpress --dbuser=root --allow-root
 /var/www/html/wp-cli core install --url=http://localhost:8080/wordpress/ --title=WordPress --admin_user=admin --admin_password=admin123 --admin_email=somefakeemail@fakedomain.com --allow-root
+/var/www/html/wp-cli db query --allow-root "UPDATE wp_options SET option_value='http://localhost:8080' WHERE option_name IN ('siteurl', 'home')"
 
-
+/var/www/html/wp-cli plugin install woocommerce --activate --version $2
