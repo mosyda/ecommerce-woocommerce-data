62352c46146af52a278f173a31f27f4a6276e5ec
Maciej Osyda <maciejosyda@seamless.se>
2016-01-15 12:41:16 +0100

    Added WP version parameter

--- install-wc.sh
+++ install-wc.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -x
 
+if [ -z "$1" ]
+  then
+    echo "First argument should be a wordpress version (at least 3.7). No version supplied"
+fi
+
 # Wait for MySQL
 while ! mysqladmin ping -h localhost --silent; do
     sleep 1
@@ -8,7 +13,7 @@ done
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`wordpress\` CHARACTER SET utf8 COLLATE utf8_general_ci;"
 
 cd /var/www/html
-/var/www/html/wp-cli core download --version=3.5 --allow-root
+/var/www/html/wp-cli core download --version=$1 --allow-root
 /var/www/html/wp-cli core config --dbname=wordpress --dbuser=root --dbpass=root --allow-root
 /var/www/html/wp-cli core install --url=http://localhost:8888/wordpress/ --title=WordPress --admin_user=admin --admin_password=admin123 --admin_email=somefakeemail@fakedomain.com --allow-root
 
